299a96926e18057db5aaa0be272bf3f3bf4d01e1
Schaun Wheeler <schaun.wheeler@gmail.com>
2014-02-10 17:25:54 -0500

    Updated record linkage and include sql script in convenience

--- wrappers/convenience.py
+++ wrappers/convenience.py
@@ -3,6 +3,10 @@
 import os
 import inspect
 import sys
+import warnings
+import pandas as pd
+import numpy as np
+
 
 def set_module_path(module_name, guide='user', choose_max=True):
     '''Allows a custom script to be imported as a module from outside
@@ -68,3 +72,189 @@ def trim_matrix(df):
     null_mask = df.apply(lambda x: x.isnull().mean()) == 0.0
     df = df.ix[null_mask, null_mask]
     return df
+
+
+def write_dataframe(df, conn, table_name, step=None, table_key=None, 
+    table_index=None, skip_errors=True, action=['prepare', 'append'], 
+    deconflict='dataframe', flavor='sqlite', verbose=True):
+    ''' Write data frame in chunks to avoid sql connectivity issues.
+
+    df : the data frame to be written the database
+    conn : the database connection to which to write the dataframe
+    step : the number of rows from df to write at a time.
+    table_name : the table within the database to which to write the df
+    table_key : a list of columns that form unique identifiers for each row
+    table_index : a dictionary of lists, where the keys identify the names of
+        indices to be created, and the lists within the dictionary values
+        contain the names of the columns that will make up each dictionary
+    skip_errors : boolean, indicates whether to continue the upload even if 
+        errors occur
+    action : list, whether to prepare a table for upload, upload a data frame,
+        or both
+    deconflict: if 'database', remove rows from database that have the same
+        key as rows in the dataframe; if 'dataframe', remove rows in the data
+        frame that have the same key as rows in the database; if 'none', do 
+        nothing
+    flavor : passed to pandas.io.sql, indicating the flavor of SQL to expect
+    verbose : boolean, indicating whether to display informative progress
+        messages
+
+    '''
+    df = df.convert_objects(convert_numeric=True)    
+        
+    if ('append' in action) and ('prepare' not in action) and deconflict != 'none':
+        if verbose:
+            print 'Reading "%s" table to deconflict data frame.' % table_name
+
+        if len(table_key) == 1:
+            concat_statement = table_key[0]
+        elif flavor == 'mysql':
+            concat_statement = 'CONCAT(%s)' % ', '.join(table_key)
+        elif flavor in ['postgresql', 'sqllite', 'oracle']:
+            concat_statement =  '||'.join(table_key)
+        else:
+            concat_statement = '+'.join(table_key)
+            
+        previous_index = pd.io.sql.read_frame(
+            'SELECT %s from %s' % (concat_statement, table_name), conn).squeeze()
+        new_index = df[table_key].fillna('').applymap(str).apply(
+            lambda x: ''.join(x), axis=1)
+        
+        if deconflict == 'dataframe':
+            df = df[~new_index.isin(previous_index)]
+        elif deconflict == 'database':
+            to_remove = previous_index[previous_index.isin(new_index)].values
+            remove_statement = 'DELETE FROM %s WHERE %s IN (%s)' % (table_name, 
+                concat_statement, ', '.join(to_remove))
+
+            cursor = conn.cursor()
+
+            try:    
+                cursor.execute(remove_statement)
+            except conn.Error:
+                cursor.close()    
+                cursor = conn.cursor()
+                cursor.execute(remove_statement)
+            cursor.close()
+
+    if 'prepare' in action:
+        if pd.io.sql.table_exists(table_name, conn, "mysql") & verbose:
+            print 'Table named "%s" already exists.' % table_name
+            print 'Existing table will be replaced.\n'
+        
+        if verbose:
+            print 'Preparing query...'
+        
+        def check_digits(series):
+            if series.dtype.name in ['float64']:
+                diff = series - series.round(0)
+                if np.nanmax(diff) == 0.0:
+                    series = series.fillna(0.0).astype('int64')
+            return series
+
+        def check_lengths(x):
+            if x.dtype == np.dtype('O'):
+                out = x.str.len().max()
+            else:
+                out = x.apply(str).str.len().max()
+            return out
+        
+        df = df.apply(check_digits)    
+        bool_cols = df.dtypes == 'bool'
+        df.ix[:,bool_cols] = df.ix[:,bool_cols].apply(
+            lambda x: x.astype('int64'))    
+        
+        datatype_dict = {np.dtype('float64'):'FLOAT(%d)',
+                         np.dtype('int64'):'INT(%d)',
+                         np.dtype('O'):'VARCHAR(%d)'}    
+        sql_dtypes = df.dtypes.map(datatype_dict)
+        col_maxlengths = df.apply(check_lengths)
+        
+        sql_dtypes = pd.Series(
+            [sql_dtypes[i] % col_maxlengths[i] for i in range(sql_dtypes.shape[0])],
+            index = sql_dtypes.index) 
+        
+        sql_dtypes = pd.DataFrame({
+            'column_name': sql_dtypes.index.values,
+            'data_types': sql_dtypes,
+            'not_null': np.where(sql_dtypes.index.isin(table_key), 'NOT NULL', '')
+        })    
+        
+        query = [
+            'DROP TABLE IF EXISTS %s;' % table_name,
+            'SHOW WARNINGS;',
+             #'SET GLOBAL max_allowed_packet=1073741824;'
+            'CREATE  TABLE IF NOT EXISTS %s (' % table_name]
+        
+        for i in range(sql_dtypes.shape[0]):
+            query.append(
+                '%s %s %s REFERENCES %s (%s),' % (
+                    sql_dtypes['column_name'][i], sql_dtypes['data_types'][i], 
+                    sql_dtypes['not_null'][i], table_name, 
+                    sql_dtypes['column_name'][i]))
+    
+        query.append('created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,')
+        query.append('updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,')
+    
+        if table_key is None:
+            table_key = df.columns[0]
+    
+        query.append('PRIMARY KEY (%s))' % ', '.join(table_key))
+        query.append('CHARSET utf8 COLLATE utf8_general_ci;')
+    
+        if table_index is not None:        
+            for idx, cols in table_index.iteritems():
+                query.append('CREATE INDEX %s on %s (%s);' % (idx, table_name, 
+                             ', '.join(cols)))
+    
+        query.append('SHOW WARNINGS;')
+        
+        query = ' '.join(query)
+        
+        cursor = conn.cursor()
+            
+        try:    
+            cursor.execute(query)
+        except conn.Error:
+            cursor.close()    
+            cursor = conn.cursor()
+            cursor.execute(query)
+        cursor.close()
+    
+    if 'append' in action:
+
+        df = df.where((pd.notnull(df)), None)
+        
+        if step is None:
+            step = df.shape[0] - 1
+        
+        if verbose:
+            print 'Executing query...'
+        
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', conn.Warning)
+            if verbose:
+                print 'Records (out of %d) written to database:' % (df.shape[0])
+            for i in range(0, df.shape[0], step): 
+                if skip_errors:                            
+                    try:
+                        pd.io.sql.write_frame(df[i:(i + step)], 
+                                              con=conn,
+                                              name=table_name, if_exists='append',
+                                              flavor=flavor)
+                        if verbose:
+                            print np.where((i + step) < df.shape[0], (i + step), 
+                                           df.shape[0]),
+                    except conn.Error, e:
+                        if verbose:
+                            print "Error %d: %s" % (e.args[0], e.args[1])
+                    
+                else:
+                    pd.io.sql.write_frame(df[i:(i + step)], 
+                                              con=conn,
+                                              name=table_name, if_exists='append',
+                                              flavor=flavor)
+                    if verbose:
+                        print np.where((i + step) < df.shape[0], (i + step), 
+                                       df.shape[0]),
+            print ''
--- wrappers/record_linkage.py
+++ wrappers/record_linkage.py
@@ -24,9 +24,10 @@ def log1p_difference_comparator(field_1, field_2):
     return log1p_diff
 
 # Function to convert data frame to hashable dictonary
-def dataframe_to_frozendict(df, id_col, to_string=False):
+def dataframe_to_recorddict(df, df_tag=None, id_col=None, to_string=False):
     '''
     df : data frame to convert to a frozen dictionary
+    df_tag : an identifier for the data frame itself, to append to id values
     id_col : column of unique identifiers; if None (default), the data frame
         index will be used
     to_string : boolean, indicating whether to convert all values to type str
@@ -37,6 +38,9 @@ def dataframe_to_frozendict(df, id_col, to_string=False):
         id_values = df[id_col].values
     else:
         id_values = df.reset_index().index.values
+    
+    if df_tag is not None:
+        id_values = [df_tag + str(x) for x in id_values]
 
     colnames = df.columns.values
 
@@ -46,19 +50,17 @@ def dataframe_to_frozendict(df, id_col, to_string=False):
         df = df.applymap(str)
 
     df= df.groupby(id_values)
-    df = df.apply(lambda x: dedupe.core.frozendict(zip(colnames, x.values[0])))
-    df.index = [int(x) for x in df.index.values]
+    df = df.apply(lambda x: dict(zip(colnames, x.values[0])))
+    df.index = [x for x in df.index.values]
 
     df_d = df.to_dict()
 
     return df_d
 
 
-def split_by_exact_matches(dataframe, columns, group_label='group', tag=None):
+def group_by_exact_matches(dataframe, columns, group_label='group', tag=None):
     '''
-    This function splits a dataframe into two dataframes - one containing
-    all records that have duplicate values on specified columns, and
-    another containing records for which there are no duplicate values.
+    Group records that are exact duplicates on specified columns.
     
     dataframe : the data frame to be split
     columns : columns along which to look for duplicate values
@@ -71,7 +73,10 @@ def split_by_exact_matches(dataframe, columns, group_label='group', tag=None):
     if type(columns) == str:
         columns = [columns]
 
-    df = dataframe.copy()
+    if type(dataframe) == list:
+        df = dataframe[0].copy()
+    else:
+        df = dataframe.copy()
 
     topdown = df[columns].apply(
         lambda x: x.duplicated(take_last=False))
@@ -81,33 +86,45 @@ def split_by_exact_matches(dataframe, columns, group_label='group', tag=None):
     
     duplicate_index = exact_matches.apply(np.sum, axis=1) > 0
     
-    duplicates = df[duplicate_index]
-    not_duplicates = df[~duplicate_index]
+    if duplicate_index.sum() > 0:
+        duplicates = df[duplicate_index]
+        not_duplicates = df[~duplicate_index]
     
-    duplicates[group_label] = duplicates.groupby(columns
-        ).grouper.group_info[0]
+        replacements = pd.Series(
+            duplicates.groupby(columns).grouper.group_info[0], 
+            index=duplicates.index)
+
+        if tag is not None:
+            replacements = replacements.apply(str) + tag
+
+        duplicates[group_label] = replacements
+        
+        df = pd.concat([duplicates, not_duplicates])
+    else:
+        df[group_label] = None
     
-    if tag is not None:
-        duplicates.ix[:,group_label] = duplicates[group_label].apply(str) + tag
-    
-    return [duplicates, not_duplicates]
+    return df
 
 
 def dataframe_linkage(records, settings, id_col=None, to_string=False,
-    records_sample=0.01, training_file=None, training='append',
-    settings_output=None, threshold_sample=1.0, recall_weight=1.5,
-    split_output=False, override_columns=None, ppc=1, uncovered_dupes=1,
-    verbose=True):
+    reset_index=True, records_sample=0.01, training_file=None, training='append',
+    settings_output=None, threshold=None, recall_weight=1.5,
+    split_output=False, override_columns=None, exclude_exact_matches=True,
+    ppc=1, uncovered_dupes=1, verbose=True):
     '''
-    records : data frame or list of data frames to convert to a frozen dictionary;
+    records : data frame or list of data frames to convert to dictionaries;
         columns to be used in records linkage must be consistently named across
-        data frames
+        data frames; if a list or dictionary or two dataframes is passed to
+        records, the two dataframes will be linked. Otherwise, the function
+        assumes a single dataframe and will label duplicates
     settings : file or dictionary containing settings to pass to dedupe module
     id_col: name of column containing unique identifiers (passed to
         `dataframe_to_frozendict`); id_col, like all columns, must be consistent
         across data frames
     to_string : boolean, indicating whether to convert all values to type str;
         passed to `dataframe_to_frozendict`
+    reset_index : reset index of data frames as a precaution to make sure each
+        row has a unique identifier
     records_sample : number of random pairs to pull from data for training; if less
         than 1, sample size will equal sample size times the number of possible
         combinations of all rows across data frames
@@ -121,6 +138,9 @@ def dataframe_linkage(records, settings, id_col=None, to_string=False,
     recall_weight : weight to determin threshold to maximize a weighted average of
         precision and recall (recall_weight==2 means we care twice as much about
         recall as we do precision)
+    threshold : float between 0.0 and 1.0 indicating cutoff point in logistic
+        regression predictons for determining matches. If None, threshold will
+        be determined algorithmically.
     split_output : boolean, indicating whether to return a list of data frames
         corresponding to the structure of the input list; if split_output==False,
         a single data frame will be returned with a 'data_frame_id' column
@@ -129,6 +149,8 @@ def dataframe_linkage(records, settings, id_col=None, to_string=False,
     override_columns : a columns of list of columns that automatically trigger
         a match; if two records match on all override_columns, they are labelled
         as a match and are not processed through the dedupe algorithms
+    exclude_exact_matches : boolean, indicating whether to leave exact matches
+        from override_columns out of the records for fuzzy matching
     ppc : float between 0.0 and 1.0; the proportion of all possible pairs a 
         predicate is allowed to cover. If a predicate puts together a fraction 
         of possible pairs greater than ppc, it will be removed from consideration. 
@@ -141,112 +163,171 @@ def dataframe_linkage(records, settings, id_col=None, to_string=False,
 
     '''
 
-    # Instantiate Dedupe class
-    deduper = dedupe.Dedupe(settings)
+    if type(settings) is dict:
+        settings = dict(settings)
+
+    if training_file is not None:
+        if (not os.path.exists(training_file)) & (training == 'none'):
+            raise Exception('must supply training_file or do active training')
+        elif os.path.exists(training_file) & (training == 'replace'):
+            os.remove(training_file)
+
+    record_type = type(records)
 
     # Convert all data frames to frozen dictionaries
     if verbose:
-        print 'prepare data frames for linkage...'
-    record_type = type(records)
+        print 'preparing records for linkage...'
+    
     if record_type == dict:
-        input_ids = {x:x for x in records.keys()}
+        input_ids = [(x+'_') for x in records.keys()]
+        records = [x for x in records.values()]
     elif record_type == list:
-        input_ids = range(len(records))
-    else:
-        input_ids = 'single data frame'
-    for i in input_ids:
-        records[i]['data_frame_id'] = input_ids[i]
-
-    if id_col is not None:
-        records = pd.concat(records, ignore_index=True).set_index(id_col, drop=False)
-    else:
-        records = pd.concat(records, ignore_index=True).reset_index()        
+        input_ids = ['first_', 'second_']
+    elif record_type == pd.DataFrame:
+        input_ids = [None]
+        records = [records]
+        
 
     if override_columns is not None: 
-        duplicates, records = split_by_exact_matches(records, 
+        
+        records = pd.concat(records, keys=input_ids)
+
+        records = group_by_exact_matches(dataframe=records, 
             columns=override_columns, group_label='cluster_id', tag='_exact')
+        
+        if exclude_exact_matches:
+            exact_dupes = records[records['cluster_id'].notnull()]
+            records = records[records['cluster_id'].isnull()]
 
-    record_dicts = dataframe_to_frozendict(records, id_col=id_col,
-        to_string=to_string)
+        if len(input_ids) == 2:
+            records = [records[records.index.get_level_values(0) == x].copy() 
+                for x in input_ids]
+            exact_dupes = [exact_dupes[exact_dupes.index.get_level_values(0) == x].copy() 
+                for x in input_ids]
+        else:
+            records = [records]
+            exact_dupes = [exact_dupes]
 
-    if type(settings) == dict:
+    if id_col is not None:
+        records = [x.set_index(id_col, drop=False) for x in records]
+    elif reset_index:
+        records = [x.reset_index(drop=True) for x in records]
 
-        if records_sample < 1:
+    for i in range(len(records)):
+        records[i].index = [str(x) for x in records[i].index]
 
-            n_records = len(record_dicts)
+    record_dicts = []
+    for i in range(len(records)):
+        record_d = dataframe_to_recorddict(records[i], id_col=id_col,
+            df_tag = input_ids[i], to_string=to_string)
+        record_dicts.append(record_d)
+
+    if records_sample < 1:
+        n_dicts = len(record_dicts)
+
+        if n_dicts == 1:
+            n_records = sum([len(x) for x in record_dicts])
             n_combinations = n_records * (n_records - 1) / 2
-            records_sample = int(np.ceil(n_combinations * records_sample))
+        else:
+            n_combinations = np.product([len(x) for x in record_dicts])
 
-        # Set up training data/files
+        records_sample = int(np.ceil(n_combinations * records_sample))
+    
+    # Instantiate appropriate Dedupe class
+    if len(records) == 2:
+        linker = dedupe.api.RecordLink(settings)
+    else:
+        linker = dedupe.Dedupe(settings)
+
+    if os.path.exists(training_file):
+        if verbose:
+            print 'reading in existing training file...'
+        linker.readTraining(training_file)
+    
+    if type(settings) is dict:
+        
         if training != 'none':
-            if os.path.exists(training_file) & (training == 'append'):
-                deduper._initializeTraining(training_file)
-
+            # Sample data for active learning
+    
             if verbose:
                 print 'randomly sampling records to perform active labelling...'
-            data_sample = dedupe.dataSample(record_dicts, records_sample)
-
+        
+            if len(records) == 2:
+                linker.sample(record_dicts[0], record_dicts[1], records_sample)
+            else:
+                linker.sample(record_dicts[0], records_sample)
+        
             if verbose:
-                print 'perform active labeling...'
-            deduper.train(data_sample, dedupe.training.consoleLabel)
-        elif os.path.exists(training_file):
-            deduper.train(data_sample, training_file)
-        else:
-            Exception('no training information (file or active labelling) provided')
-
+                print 'initiating active labeling...'
+            
+            dedupe.consoleLabel(linker)
+    
+        if verbose:
+            print 'training model...'
+        
+        linker.train(ppc=ppc, uncovered_dupes=uncovered_dupes)
+    
         if training_file is not None:
-            deduper.writeTraining(training_file)
-
-    if verbose:
-        print 'blocking...'
-    blocker = deduper.blockingFunction(ppc=ppc, uncovered_dupes=uncovered_dupes)
-
-    if settings_output is not None:
-        deduper.writeSettings(settings_output)
-
-    # Load all the original data in to memory and place them in to blocks.
-    blocked_data = dedupe.blockData(record_dicts, blocker)
-
-    # Clustering
-    if threshold_sample < 1:
-        n_blocks = len(blocked_data)
-        block_sample = np.random.choice(n_blocks, n_blocks * threshold_sample,
-            replace=True)
-        threshold = deduper.goodThreshold(
-            tuple([blocked_data[x] for x in block_sample]),
-            recall_weight=recall_weight)
-    else:
-        threshold = deduper.goodThreshold(blocked_data,
-            recall_weight=recall_weight)
-
+            linker.writeTraining(training_file)
+        if settings_output is not None:
+            linker.writeSettings(settings_output)
+    
+        if threshold == None:
+            if verbose:
+                print 'blocking...'
+            if len(records) == 2:
+                threshold = linker.threshold(record_dicts[0], record_dicts[1], 
+                    recall_weight=recall_weight)
+            else:
+                threshold = linker.threshold(record_dicts[0], 
+                    recall_weight=recall_weight)
+    
     if verbose:
         print 'clustering...'
-    clustered_dupes = deduper.duplicateClusters(blocked_data, threshold)
+
+    if len(records) == 2:
+        clustered_dupes = linker.match(record_dicts[0], record_dicts[1], 
+            threshold)
+    else:
+        clustered_dupes = linker.match(record_dicts[0], threshold)
 
     if verbose:
         print '# duplicate sets', len(clustered_dupes)
 
-    # create a 'cluster_id' column to show which records fall within the same cluster
-    clustered_dupes = [zip(y, [x]*len(y)) for (x, y) in
-        enumerate(clustered_dupes)]
-    clustered_dupes = [x for y in clustered_dupes for x in y]
-    cluster_membership = pd.Series({ x:y for (x,y) in clustered_dupes })
-    cluster_membership.index = [int(x) for x in cluster_membership.index]
-    cluster_membership = cluster_membership.reindex(
-        index=[int(x) for x in records.index])
-    cluster_membership.index = records.index
+    if len(records)==2:
+        clustered_dupes = enumerate(clustered_dupes)
+        clustered_dupes = [(y, z, x) for (x,(y,z))  in clustered_dupes]
+        record_0_dupes = pd.Series({ str(x).replace(input_ids[0], ''):z for 
+            (x,y,z) in clustered_dupes })
+        record_1_dupes = pd.Series({ str(y).replace(input_ids[1], ''):z for 
+            (x,y,z) in clustered_dupes })
+        record_0_dupes = record_0_dupes.reindex(index=records[0].index)
+        record_1_dupes = record_1_dupes.reindex(index=records[1].index)
 
-    records['cluster_id'] = cluster_membership
+        records[0]['cluster_id'] = record_0_dupes
+        records[1]['cluster_id'] = record_1_dupes
+        
+        if (override_columns is not None) & exclude_exact_matches:
+            records[0] = pd.concat([records[0], exact_dupes[0]], ignore_index=True)
+            records[1] = pd.concat([records[1], exact_dupes[1]], ignore_index=True)
     
-    if override_columns is not None:
-        records = pd.concat([records, duplicates], ignore_index=True)
+    else:
+        # create a 'cluster_id' column to show which records fall within the same cluster
+        clustered_dupes = enumerate(clustered_dupes)
+        clustered_dupes = [zip(y, [x]*len(y)) for (x, y) in clustered_dupes]
+        clustered_dupes = [x for y in clustered_dupes for x in y]
+        cluster_membership = pd.Series({ x:y for (x,y) in clustered_dupes })
+        cluster_membership.index = [str(x) for x in cluster_membership.index]
+        cluster_membership = cluster_membership.reindex(index=records[0].index)
 
-    if split_output == True:
-        if record_type == dict:
-            records = {x:records[records['data_frame_id'] == x] for x in input_ids}
-        elif record_type == list:
-            records = [records[records['data_frame_id'] == x] for x in input_ids]
-        else:
-            print 'only one data frame was input, so records cannot be split'
+        records['cluster_id'] = cluster_membership
+
+        if (override_columns is not None) & exclude_exact_matches:
+            records[0] = pd.concat([records[0], exact_dupes[0]], ignore_index=True)
+    
+    if record_type == dict:
+        records = {input_ids[i].replace('_', ''):records[i] for i in range(len(input_ids))}
+    elif record_type == pd.DataFrame:
+        records = records[0]
 
     return records
